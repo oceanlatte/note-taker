75599ac10e539d9b811dd44274aa785819b0667c
ocean <osciris.veliz@gmail.com>
2022-08-02 21:40:36 -0500

    add get route to load index.html as home page

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express');
-const app = express();
-
-const { notes } = require('./db/db.json');
+const { notes } = require('./db/db.json'); // require JSON file
 
 const PORT = process.env.PORT || 3001;
+const app = express(); // instantiate express
+const fs = require('fs'); // write file
+const path = require('path'); // works with file paths
+// api route?
+// html route?
 
+
+// ADD APP.USE middleware functions here??? 
+
+
+// get database, explicitly do I need this? 
 app.get('/api/db', (req, res) => {
-  res.send('Hello');
-  // res.sendFile(__dirname + '/index.html');
+  res.json(notes);
 })
 
-// app.get('')
+// get the homepage for the server to display INDEX.HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
