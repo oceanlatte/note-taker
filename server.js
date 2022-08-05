934e1d40a82e1903a6dee60a39359d6e1a83c583
ocean <osciris.veliz@gmail.com>
2022-08-04 19:54:47 -0500

    set up POST to handle unique ID; set up separate function to handle new note

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const { notes } = require('./db/db.json'); // require JSON file
 
 const PORT = process.env.PORT || 3001;
-const app = express(); // instantiate express
-const fs = require('fs'); // write file
 const path = require('path'); // works with file paths
+const fs = require('fs'); // write file
+const app = express(); // instantiate express
 
 // ---APP.USE middleware functions here ---
 app.use(express.static('public'));
@@ -14,11 +14,12 @@ app.use(express.urlencoded({ extended: true }));
 // express.json takes incoming POST data and parses into req.body JS obj
 app.use(express.json()); 
 
-app.post('/api/notes', (req, res) => {
-  // POST /api/notes - ADDS to the db.json, RETURN new note
-  // give each note a unique id when saved 
-  // fs read file
-})
+
+const addNewNote = (body, notesArr) => {
+  console.log(body, "body in NEW NOTE FUNC");
+  console.log(notesArr, "DB / notes array");
+}
+
 
 // GET /api/notes - should READ db.json AND RETURN all saved notes
 app.get('/api/notes', (req, res) => {
@@ -34,6 +35,26 @@ app.get('/notes', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+
+// POST /api/notes - ADDS to the db.json, RETURN new note
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body;
+  console.log({ title }, "logged destructured title in POST method");
+  
+  const newNote = {
+    title,
+    text,
+    // give each note a unique id when saved
+    // testing ID 
+    id: "123456789"
+  }
+  
+  const returnedNote = addNewNote(newNote, notes);
+  
+  // fs read file somewhere here??
+  res.json(returnedNote);
+})
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
